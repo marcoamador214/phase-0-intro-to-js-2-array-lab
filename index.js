dda09233cbb0d53e0448330f7ace8a04d7131ce3
Marco Amador <marcoamador214@gmail.com>
2022-10-11 10:52:20 -0700

    commit tree f684dbc8b13a429522564b89e63a3fac82ad63db

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+    const copyOfcats = [...cats, "Broom"]
+    return copyOfcats;
+}
+
+function prependCat(name) {
+    const copyCat = ["Arnold", ...cats]
+    return copyCat;
+}
+
+function removeLastCat() {
+    const slicedCats = cats.slice(0, cats.length-1)
+    return slicedCats;
+}
+
+function removeFirstCat() {
+    const slicedUpCats = cats.slice(1)
+    return slicedUpCats;
+}
